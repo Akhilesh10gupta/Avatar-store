9d8dc2292c20e754e9c22a50b1937a96beea8b20
guptaji <akhilshorts87@gmail.com>
2026-01-14 10:36:01 +0530

    update

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZ1VCnL844v59XoTixP3J_YVrxNmsSxrI",
-  authDomain: "gameweb-4fe8b.firebaseapp.com",
-  projectId: "gameweb-4fe8b",
-  storageBucket: "gameweb-4fe8b.firebasestorage.app",
-  messagingSenderId: "1075367080838",
-  appId: "1:1075367080838:web:d7378dfe6a33f05538f207",
-  measurementId: "G-B45VBQ76C6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
